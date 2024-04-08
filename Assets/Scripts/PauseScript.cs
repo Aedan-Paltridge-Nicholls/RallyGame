54e7bf5c4b5c58aac9e3c5ab73619989c9d148ee
Aedan H.P. Nicholls <aedan432@gmail.com>
2024-04-09 11:17:07 +1200

    try

---
+++ Assets/Scripts/PauseScript.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class PauseScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        PauseMenu.enabled = false;
+    }
+    [SerializeField] Canvas  PauseMenu;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.P) || Input.GetKeyDown(KeyCode.Escape))
+        {
+            
+            TogglePause();
+        }
+    }
+
+    void TogglePause()
+    {
+
+        bool isPaused = !Time.timeScale.Equals(0f);
+        Time.timeScale = isPaused ? 0f : 1f;
+        PauseMenu.enabled  =  (isPaused) ? true : false;
+
+        // Show/hide pause menu UI accordingly
+    }
+}
---
+++ Assets/Scripts/checkpointController.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+
+public class checkpointController : MonoBehaviour
+{   
+    [SerializeField] int CheckpointCount;
+    [SerializeField] TextMeshProUGUI CheckpointCounter;
+    [SerializeField] TextMeshProUGUI TimerTxt;
+    [SerializeField] TextMeshProUGUI FinalTime;
+    [SerializeField] Canvas FinishScreen;
+
+  
+
+    private float StageTime;
+    private int HitCheckpoints;
+    private string CheckpointCountText;
+    private bool StageStarted;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        FinishScreen.enabled = false;
+        StageStarted = false;
+        CheckpointCountText = HitCheckpoints.ToString() + " Of " + CheckpointCount.ToString() ;
+        CheckpointCounter.text = CheckpointCountText;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ( StageStarted )
+        {
+            StageTime += Time.deltaTime;
+
+            int mins = Mathf.FloorToInt(StageTime / 60);
+            int secs = Mathf.FloorToInt(StageTime % 60);
+            int tenths = Mathf.FloorToInt((StageTime * 10) % 10);
+
+            TimerTxt.text = string.Format("{0:00}:{1:00}.{2}", mins, secs, tenths);
+        }
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag.Equals("checkpoint"))
+        {
+            string log = "checkpoint hit" + HitCheckpoints.ToString(); 
+            Debug.Log(log);
+            other.enabled = false;
+            HitCheckpoints++;
+            CheckpointCountText = HitCheckpoints.ToString() + " of " + CheckpointCount.ToString();
+            CheckpointCounter.text = CheckpointCountText;
+
+        }
+        if (other.gameObject.tag.Equals("Finish"))
+        {
+            if(HitCheckpoints == CheckpointCount) 
+            { 
+                Time.timeScale = 0f;
+                string log = "Finished";
+                Debug.Log(log);
+                other.enabled = false;
+                StageStarted = false;
+                FinalTime.text = TimerTxt.text;
+               
+                FinishScreen.enabled = true;
+            }
+
+        }
+        if (other.gameObject.tag.Equals("Start"))
+        {
+            string log = "Started";
+            Debug.Log(log);
+            other.enabled = false;
+            StageStarted = true;
+        }
+    }
+}
